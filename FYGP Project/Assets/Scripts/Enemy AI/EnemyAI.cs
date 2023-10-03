e0b0a0aaf7d9d5acc1f7a8fd2cea98f0a6b9f9f5
Jin <100594091+JinScenes@users.noreply.github.com>
2023-10-03 18:33:52 +0100

    V1.0.2
    EnemyAI

---
+++ FYGP Project/Assets/Scripts/Enemy AI/EnemyAI.cs
@@ -0,0 +1,502 @@
+using System.Collections;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine;
+using Unity.VisualScripting;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public class EnemyAI : MonoBehaviour
+{
+    [Header("AI States")]
+    [Tooltip("Check if the AI is within a given range for the AI to cahse the player"),
+    SerializeField] private Movement movementState;
+    [HideInInspector] public int numberMovement;
+
+    [Tooltip("Checks if the AI meets the requirements to be destroyed"),
+    Space(5), SerializeField] private DestroyedState destroyState;
+    [HideInInspector] public int numberDestroy;
+
+    [Tooltip("Check if the UI Health Bar should be hidden depending on the state"),
+    Space(5), SerializeField] private HealthBar healthBarState;
+    [HideInInspector] public int numberHealth;
+
+    [Tooltip("Checks if the AI is ready to attack the player depending on the range from the player"),
+    Space(5), SerializeField] private AttackingState attackingState;
+    [HideInInspector] public int numberAttacks;
+
+    /// <summary>
+    /// ENUM STATES
+    /// </summary>
+
+    public enum Movement
+    {
+        Walk, Run
+    };
+
+    public enum HealthBar
+    {
+        Hide, Hidden
+    };
+
+    public enum DestroyedState
+    {
+        Enable, Disable
+    };
+
+    public enum AttackingState
+    {
+        Enable, Disable
+    };
+
+    /// <summary>
+    /// KEY COMPONENTS
+    /// </summary>
+
+    private Transform playerTransform;
+    private Transform originPos;
+    private NavMeshAgent navMesh;
+    private Slider healthBarUI;
+    private Animator anim;
+
+    /// <summary>
+    /// AI VARIABLES
+    /// </summary>
+
+    [Header("AI Stats"),
+    Tooltip("The name of the player's gameobject"),
+    SerializeField] private string playerObjectName;
+
+    [Tooltip("The origin point of the zombie's attacking point"),
+    SerializeField] private Transform attackPoint;
+
+    [Tooltip("The player's layermask"),
+    SerializeField] private LayerMask playerMask;
+
+    [Range(0, 200), Tooltip("The enemy's health value")]
+    public float health;
+
+    [Range(0, 100), Tooltip("The requirement to trigger the enemy to switch movement states")]
+    public float loudness;
+
+    [Range(0, 100), Tooltip("The amount of damage given to a player")]
+    public float damage;
+
+    [Range(0, 10), Tooltip("How far the AI can see the player in order to chase them")]
+    public float sightRange;
+
+    [Range(0, 5), Tooltip("The range in which the enemy attacks the player")]
+    public float attackRange;
+
+    [Range(0, 1), Tooltip("How fast the zombie is attacking the player")]
+    public float attackRate;
+
+    [Range(0, 10), Tooltip("If the player is in the given range, the zombie will chase the player")]
+    public float detectionRange;
+
+    [Range(0, 1), Tooltip("The minimum time for the next step")]
+    public float minStepInterval;
+
+    /// <summary>
+    /// AUDIO
+    /// </summary>
+
+    [Header("Audio")]
+    [SerializeField] private string[] footstepAudioNames;
+    [SerializeField] private string[] attackAudioNames;
+    [SerializeField] private string[] groanAudioNames;
+
+    [SerializeField] private string[] deathAudioNames;
+    [SerializeField] private string[] hitAudioNames;
+
+    [Range(0f, 5f), Tooltip("The minimum interval for playing groan noise"),
+    SerializeField] private float minGroanInterval;
+
+    [Range(0f, 20f), Tooltip("The maximum interval for playing groan noise"),
+    SerializeField] private float maxGroanInterval;
+
+    /// <summary>
+    /// BOOLEAN CHECK
+    /// </summary>
+
+    private float nextAttackTime = 0f;
+    private float lookAtSpeed = 3f;
+    private float timeSinceLastStep;
+    private float distanceToPlayer;
+    private float nextGroanTime;
+    private float randomDestroy;
+    private float readyTime;
+
+    [HideInInspector] public bool IsMove;
+    [HideInInspector] public bool Spawn;
+    [HideInInspector] public bool canHit;
+    [HideInInspector] public bool canSee;
+
+    private bool isDead = false;
+    private bool ready;
+
+    private void Start()
+    {
+        StartingsStates();
+        VariableSetup();
+    }
+    private void Update()
+    {
+        if (!isDead)
+        {
+            //anim.SetLayerWeight(anim.GetLayerIndex("UpperBody"), 1);
+
+            ReadyBoolSwitch();
+            HearingRange();
+            AttackDistanceChecker();
+            //AIHealth();
+            CheckDistance();
+            AngleSights();
+            WithinRange();
+            GroanAudio();
+
+            //ENUM STATES
+            MovementFunction();
+            DeathFunction();
+            //HealthUIFunction();
+        }
+    }
+
+    private void VariableSetup()
+    {
+        //healthBarUI = transform.Find("HealthBar_Canvas/Slider").GetComponent<Slider>();
+        originPos = transform.Find("Pos").GetComponent<Transform>();
+        navMesh = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        playerTransform = GameObject.Find("Player").transform;
+        nextGroanTime = Time.time + Random.Range(minGroanInterval, maxGroanInterval);
+        randomDestroy = Random.Range(5f, 8f);
+        canHit = false;
+    }
+
+    private void StartingsStates()
+    {
+        if (Spawn == false)
+        {
+            switch (attackingState)
+            {
+                case AttackingState.Enable:
+                    {
+                        numberAttacks = 0;
+                        break;
+                    }
+
+                case AttackingState.Disable:
+                    {
+                        numberAttacks = 1;
+                        break;
+                    }
+            }
+
+            switch (movementState)
+            {
+                case Movement.Walk:
+                    {
+                        numberMovement = 0;
+                        break;
+                    }
+
+                case Movement.Run:
+                    {
+                        numberMovement = 1;
+                        break;
+                    }
+            }
+
+            switch (healthBarState)
+            {
+                case HealthBar.Hide:
+                    {
+                        numberHealth = 1;
+                        break;
+                    }
+
+                case HealthBar.Hidden:
+                    {
+                        numberHealth = 1;
+                        break;
+                    }
+            }
+
+            switch (destroyState)
+            {
+                case DestroyedState.Enable:
+                    {
+                        numberDestroy = 0;
+                        break;
+                    }
+
+                case DestroyedState.Disable:
+                    {
+                        numberDestroy = 1;
+                        break;
+                    }
+            }
+        }
+
+        if (numberAttacks == 0)
+        {
+            canHit = true;
+        }
+
+        if (numberAttacks == 1)
+        {
+            canHit = false;
+        }
+    }
+
+    private void MovementFunction()
+    {
+        if (numberMovement == 0)
+        {
+            if (IsMove == true)
+            {
+                navMesh.speed = 1.5f;
+                navMesh.destination = playerTransform.position;
+                anim.SetBool("Walk", true);
+
+                if (timeSinceLastStep >= minStepInterval)
+                {
+                    //AUDIO MANAGER
+                    timeSinceLastStep = 0f;
+                }
+
+                timeSinceLastStep += Time.deltaTime;
+            }
+
+            if (IsMove == false)
+            {
+                anim.SetBool("Walk", false);
+                navMesh.speed = 0;
+            }
+        }
+    }
+
+    private void DeathFunction()
+    {
+        if (health < 0f)
+        {
+            //DEATH AUDIO
+            isDead = true;
+
+        }
+    }
+
+    private void HealthUIFunction()
+    {
+        if (numberHealth == 0 && health > 0)
+        {
+            healthBarUI.gameObject.SetActive(true);
+        }
+
+        if (numberHealth == 1 && health > 0)
+        {
+            healthBarUI.gameObject.SetActive(false);
+        }
+    }
+
+    private void CheckDistance()
+    {
+        distanceToPlayer = Vector3.Distance(playerTransform.position, transform.position);
+    }
+
+    private void AngleSights()
+    {
+        Vector3 targetDir = playerTransform.position - transform.position;
+        float angle = Vector3.Angle(targetDir, transform.forward);
+
+        if (angle < 45f && distanceToPlayer < 10f && canSee == true)
+        {
+            canHit = true;
+        }
+    }
+
+    private void LookAtTarget()
+    {
+        if (health > 0f)
+        {
+            var rotation = Quaternion.LookRotation(playerTransform.position - transform.position);
+            rotation.x = 0;
+            rotation.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * lookAtSpeed);
+        }
+    }
+
+    private IEnumerator TimeToDestroy()
+    {
+        yield return new WaitForSeconds(randomDestroy);
+        Destroy(gameObject);
+    }
+
+    private void WithinRange()
+    {
+        Collider[] playerCheckRange = Physics.OverlapSphere(transform.position, detectionRange);
+        foreach (Collider player in playerCheckRange)
+        {
+            if (player.gameObject.name == playerObjectName && numberMovement != 0)
+            {
+                numberMovement = 0;
+                IsMove = true;
+            }
+        }
+    }
+
+    private void DuringDeath()
+    {
+        anim.SetBool("Attack", false);
+        anim.SetBool("Death", true);
+        int RandomDeath = Random.Range(1, 5);
+        anim.SetInteger("Death_Int", RandomDeath);
+        this.GetComponent<NavMeshAgent>().enabled = false;
+        anim.enabled = false;
+        healthBarUI.gameObject.SetActive(false);
+
+        if (numberDestroy == 0)
+        {
+            StartCoroutine(TimeToDestroy());
+        }
+    }
+
+    private void AttackDistanceChecker()
+    {
+        if (distanceToPlayer < 2 && canHit == true)
+        {
+            LookAtTarget();
+        }
+    }
+
+    private void CheckAttack()
+    {
+        Collider[] hitPlayer = Physics.OverlapSphere(attackPoint.position, attackRange, playerMask);
+
+        foreach(Collider player in hitPlayer)
+        {
+            if(Time.time >= nextAttackTime)
+            {
+                anim.SetBool("Attack", true);
+                //ATTACKING AUDIO
+                //PLAYER HEALTH SYSTEM
+                IsMove = false;
+                lookAtSpeed = 2f;
+            }
+            else
+            {
+                anim.SetBool("Attack", false);
+                if (canHit == true)
+                {
+                    IsMove = true;
+                    lookAtSpeed = 3f;
+                }
+            }
+        }
+    }
+
+    public void EventAttack()
+    {
+        RaycastHit hit;
+        float range = 1.3f;
+        Debug.DrawRay(originPos.position, originPos.transform.TransformDirection(Vector3.forward)
+            * range, Color.red);
+
+        if (Physics.Raycast(originPos.position, originPos.transform.TransformDirection(Vector3.forward), 
+            out hit, range))
+        {
+            if (hit.transform.gameObject.name == playerObjectName)
+            {
+                hit.transform.gameObject.SendMessage("ApplyDamage", damage);
+            }
+        }
+    }
+
+    public void AIHealth()
+    {
+        healthBarUI.value = health / 100;
+
+        if (health > 0)
+        {
+            CheckAttack();
+            
+        }
+    }
+
+    private void HearingRange()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, loudness);
+        foreach(Collider hitCol in hitColliders)
+        {
+            if(hitCol.gameObject.name == playerObjectName)
+            {
+                ready = true;
+            }
+        }
+    }
+
+    private void VisualisePlayer()
+    {
+        RaycastHit hit;
+        Vector3 fromPos = originPos.transform.position;
+        Vector3 toPos = new Vector3(playerTransform.transform.position.x, playerTransform.transform.position.y + 1,
+            playerTransform.transform.position.z);
+        Vector3 dir = toPos - fromPos;
+
+        Debug.DrawRay(originPos.position, dir, Color.cyan);
+
+        if (Physics.Raycast(originPos.position, dir, out hit, sightRange))
+        {
+            if (hit.transform.gameObject.name == playerObjectName)
+            {
+                canSee = true;
+            }
+            if(hit.transform.gameObject.name != playerObjectName)
+            {
+                canSee = false;
+            }
+        }
+    }
+
+    private void GroanAudio()
+    {
+        if (Time.time > nextGroanTime)
+        {
+            //GROAN AUDIO
+            nextGroanTime = Time.time + Random.Range(minGroanInterval, maxGroanInterval);
+        }
+    }
+
+    private void ReadyBoolSwitch()
+    {
+        if (readyTime > 0f)
+        {
+            readyTime -= Time.deltaTime;
+        }
+        if (readyTime <= 0f)
+        {
+            ready = false;
+            readyTime = 1f;
+        }
+    }
+
+    public void EnemyDamage(float damage)
+    {
+        //AUDIO BEING HIT
+        health -= damage;
+        IsMove = true;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, loudness);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+    }
+}
--- FYGP Project/Assets/Scripts/Player/PlayerController.cs
+++ FYGP Project/Assets/Scripts/Player/PlayerController.cs
@@ -9,8 +9,8 @@ public class PlayerController : MonoBehaviour
     [Range(0, 1)] public float movementSmoothness;
 
     private Vector3 currentVel;
-    private Vector2 leftStickInput;
-    private Vector2 rightStickInput;
+    private Vector2 leftJoystickInput;
+    private Vector2 rightJoystickInput;
 
     private Rigidbody rb;
 
@@ -24,37 +24,41 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
 
         //INPUT MOVEMENT CALLBACK
-        input.Player.Movement.performed += ctx => leftStickInput = ctx.ReadValue<Vector2>();
-        input.Player.Movement.canceled += ctx => leftStickInput = Vector2.zero;
+        input.Player.Movement.performed += ctx => leftJoystickInput = ctx.ReadValue<Vector2>();
+        input.Player.Movement.canceled += ctx => leftJoystickInput = Vector2.zero;
 
         //INPUT ROTATION CALLBACK
-        input.Player.Rotation.performed += ctx => rightStickInput = ctx.ReadValue<Vector2>();
+        input.Player.Rotation.performed += ctx => rightJoystickInput = ctx.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
     }
 
     private void Update()
     {
-        PlayerMovement();
         PlayerRotation();
-        UpdateController();
+        UpdateGamepad();
     }
 
-    private void UpdateController()
+    private void UpdateGamepad()
     {
-        leftStickInput = input.Player.Movement.ReadValue<Vector2>();
-        rightStickInput = input.Player.Rotation.ReadValue<Vector2>();
+        leftJoystickInput = input.Player.Movement.ReadValue<Vector2>();
+        rightJoystickInput = input.Player.Rotation.ReadValue<Vector2>();
     }
 
     private void PlayerMovement()
     {
-        Vector3 targetVelocity = new Vector3(leftStickInput.x, 0, leftStickInput.y) * speed;
+        Vector3 targetVelocity = new Vector3(leftJoystickInput.x, 0, leftJoystickInput.y) * speed;
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref currentVel, movementSmoothness);
     }
 
     private void PlayerRotation()
     {
-        if(rightStickInput.magnitude > 0.1f)
+        if(rightJoystickInput.magnitude > 0.1f)
         {
-            float angle = Mathf.Atan2(rightStickInput.x, rightStickInput.y) * Mathf.Rad2Deg;
+            float angle = Mathf.Atan2(rightJoystickInput.x, rightJoystickInput.y) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0, angle, 0);
         }
     }
