596cefae1014362b6dbbfdf90a691fe12c94cea4
KidNoobly <kidnoobly@gmail.com>
2023-11-21 20:20:02 +0000

    Detailed Consumption Mechanics + All Item Consumption
    Got the fortnite thing

---
+++ FYGP Project/Assets/Inventory/Scripts/FollowObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public GameObject followObject;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
