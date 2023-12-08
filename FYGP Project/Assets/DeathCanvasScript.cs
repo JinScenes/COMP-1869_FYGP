345978b6f81199da4681d08147be55b94c4e27a8
Jin <100594091+JinScenes@users.noreply.github.com>
2023-12-08 11:42:10 +0000

    we're doneeede

---
+++ FYGP Project/Assets/DeathCanvasScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DeathCanvasScript : MonoBehaviour
+{
+    public Button retryButton;
+    public Button quitButton;
+
+    public void RetryGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public  void QuitGame()
+    {
+        Application.Quit();
+    }
+}
